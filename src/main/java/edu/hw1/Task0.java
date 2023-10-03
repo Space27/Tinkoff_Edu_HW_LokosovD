22faee40000f8d7dc0b6db0d5e921e87e26ead90
Space_27 <lokosov.danil@yandex.ru>
2023-10-03 23:53:02 +0300

    fixes Task0 and Task1

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -2,10 +2,13 @@ package edu.hw1;
 
 import org.apache.logging.log4j.LogManager;
 
-public class Task0 {
+public final class Task0 {
     private final static org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger();
 
-    public static void main(String[] args) {
+    private Task0() {
+    }
+
+    public static void helloWorld() {
         LOGGER.info("Привет, мир!");
     }
 }
