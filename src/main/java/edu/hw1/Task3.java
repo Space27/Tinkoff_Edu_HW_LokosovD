00396be703f9a3ee64d4a321da1b5b30515f74e0
Space_27 <lokosov.danil@yandex.ru>
2023-10-04 21:43:26 +0300

    added Task3

---
+++ src/main/java/edu/hw1/Task3.java
@@ -0,0 +1,35 @@
+package edu.hw1;
+
+public final class Task3 {
+    private Task3() {
+    }
+
+    private static int arrMin(int[] arr) {
+        int min = arr[0];
+
+        for (var i : arr) {
+            if (i < min) {
+                min = i;
+            }
+        }
+
+        return min;
+    }
+
+    private static int arrMax(int[] arr) {
+        int max = arr[0];
+
+        for (var i : arr) {
+            if (i > max) {
+                max = i;
+            }
+        }
+
+        return max;
+    }
+
+    public static boolean isNestable(int[] firstArr, int[] secondArr) {
+        /* вместо написанных методов можно было использовать встроенные методы класса Arrays */
+        return arrMin(firstArr) > arrMin(secondArr) && arrMax(firstArr) < arrMax(secondArr);
+    }
+}
---
+++ src/test/java/edu/hw1/Task3Test.java
@@ -0,0 +1,127 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task3Test {
+    @Test
+    @DisplayName("Простая вложенность")
+    void simpleNesting() {
+        int[] firstArray = {1, 2};
+        int[] secondArray = {0, 3};
+
+        boolean nestable = Task3.isNestable(firstArray, secondArray);
+
+        assertThat(nestable)
+            .isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Одинаковые массивы")
+    void equalNesting() {
+        int[] firstArray = {0, 1, 2, 3};
+        int[] secondArray = {0, 1, 2, 3};
+
+        boolean nestable = Task3.isNestable(firstArray, secondArray);
+
+        assertThat(nestable)
+            .isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("Одинаковый максимальный элемент массивов")
+    void maxEqualNesting() {
+        int[] firstArray = {2, 3};
+        int[] secondArray = {1, 2, 3};
+
+        boolean nestable = Task3.isNestable(firstArray, secondArray);
+
+        assertThat(nestable)
+            .isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("Одинаковый минимальный элемент массивов")
+    void minEqualNesting() {
+        int[] firstArray = {1, 2};
+        int[] secondArray = {1, 2, 3};
+
+        boolean nestable = Task3.isNestable(firstArray, secondArray);
+
+        assertThat(nestable)
+            .isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("Отрицательный минимальный элемент массивов")
+    void negMinNesting() {
+        int[] firstArray = {0, -5, 1, 2};
+        int[] secondArray = {2, 3, -7};
+
+        boolean nestable = Task3.isNestable(firstArray, secondArray);
+
+        assertThat(nestable)
+            .isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Отрицательные массивы")
+    void negNesting() {
+        int[] firstArray = {-10, -5, -3, -2};
+        int[] secondArray = {-1, -20, -7};
+
+        boolean nestable = Task3.isNestable(firstArray, secondArray);
+
+        assertThat(nestable)
+            .isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Отрицательные массивы с одинаковым максимумом")
+    void negMaxEqualNesting() {
+        int[] firstArray = {-10, -5, -3, -2};
+        int[] secondArray = {-2, -20, -7};
+
+        boolean nestable = Task3.isNestable(firstArray, secondArray);
+
+        assertThat(nestable)
+            .isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("Первый массив больше второго")
+    void firstHigherNesting() {
+        int[] firstArray = {100, -20, 30, -10, 1};
+        int[] secondArray = {90, 50, 30};
+
+        boolean nestable = Task3.isNestable(firstArray, secondArray);
+
+        assertThat(nestable)
+            .isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("Массивы не пересекаются")
+    void notIntersectNesting() {
+        int[] firstArray = {30, 100, 20};
+        int[] secondArray = {1, 3, 5, 8, 13};
+
+        boolean nestable = Task3.isNestable(firstArray, secondArray);
+
+        assertThat(nestable)
+            .isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("Максимум одного равен минимуму другого")
+    void maxMinNesting() {
+        int[] firstArray = {30, 100, 20};
+        int[] secondArray = {349, 200, 100};
+
+        boolean nestable = Task3.isNestable(firstArray, secondArray);
+
+        assertThat(nestable)
+            .isEqualTo(false);
+    }
+}
