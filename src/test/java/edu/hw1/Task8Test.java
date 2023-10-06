c39d090d289dde691bf1bf9b2bfcb97875c8c4e7
Space_27 <lokosov.danil@yandex.ru>
2023-10-06 20:36:14 +0300

    added Task8

---
+++ src/test/java/edu/hw1/Task8Test.java
@@ -0,0 +1,167 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task8Test {
+    @Test
+    @DisplayName("Коней нет")
+    void noKnights() {
+        int[][] chessBoard = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        boolean isKnightJumpsKnight = Task8.knightBoardCapture(chessBoard);
+
+        assertThat(isKnightJumpsKnight)
+            .isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Один конь")
+    void oneKnights() {
+        int[][] chessBoard = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        boolean isKnightJumpsKnight = Task8.knightBoardCapture(chessBoard);
+
+        assertThat(isKnightJumpsKnight)
+            .isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Два нейтральных коня")
+    void twoNeutralKnights() {
+        int[][] chessBoard = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        boolean isKnightJumpsKnight = Task8.knightBoardCapture(chessBoard);
+
+        assertThat(isKnightJumpsKnight)
+            .isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Два бьющих коня")
+    void twoBeatingKnights() {
+        int[][] chessBoard = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        boolean isKnightJumpsKnight = Task8.knightBoardCapture(chessBoard);
+
+        assertThat(isKnightJumpsKnight)
+            .isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("Кони в шахматном порядке")
+    void chessOrderKnights() {
+        int[][] chessBoard = {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1}
+        };
+
+        boolean isKnightJumpsKnight = Task8.knightBoardCapture(chessBoard);
+
+        assertThat(isKnightJumpsKnight)
+            .isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Сложный порядок коней")
+    void hardOrderKnights() {
+        int[][] chessBoard = {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+
+        boolean isKnightJumpsKnight = Task8.knightBoardCapture(chessBoard);
+
+        assertThat(isKnightJumpsKnight)
+            .isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("Кони на границах")
+    void borderKnights() {
+        int[][] chessBoard = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0}
+        };
+
+        boolean isKnightJumpsKnight = Task8.knightBoardCapture(chessBoard);
+
+        assertThat(isKnightJumpsKnight)
+            .isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("Конь в углу")
+    void cornerKnights() {
+        int[][] chessBoard = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1}
+        };
+
+        boolean isKnightJumpsKnight = Task8.knightBoardCapture(chessBoard);
+
+        assertThat(isKnightJumpsKnight)
+            .isEqualTo(false);
+    }
+}
