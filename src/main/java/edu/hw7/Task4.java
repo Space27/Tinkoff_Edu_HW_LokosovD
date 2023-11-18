e0733b4386cb8868904e98bf527404611ae50bec
Space_27 <lokosov.danil@yandex.ru>
2023-11-18 22:11:29 +0300

    fixes Task4

--- src/main/java/edu/hw7/Task4.java
+++ src/main/java/edu/hw7/Task4.java
@@ -14,7 +14,7 @@ public final class Task4 {
     private static final double CIRCLE_RAD_SQUARE = 0.25;
     private static final double K = 4.;
     private static final int THREAD_COUNT = 6;
-    private static final int TEST_COUNT = 5;
+    private static final int TEST_COUNT = 10;
 
     public static double calculatePi(long iterations) {
         final ThreadLocalRandom RANDOM = ThreadLocalRandom.current();
