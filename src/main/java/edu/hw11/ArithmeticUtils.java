b5c5b61d00559953c983a2854ce4988398c52ee2
Daniil Lokosov <lokosov.danil@yandex.ru>
2023-12-13 18:58:26 +0300

    added Task2

---
+++ src/main/java/edu/hw11/ArithmeticUtils.java
@@ -0,0 +1,15 @@
+package edu.hw11;
+
+public final class ArithmeticUtils {
+
+    private ArithmeticUtils() {
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+}
